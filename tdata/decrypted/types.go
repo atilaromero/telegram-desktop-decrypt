16d8a1218b8bb085aea1e6561204bd1c0746cd7e
Atila Romero <atilaromero@gmail.com>
2019-03-07 20:12:30 -0300

    gofmt

--- tdata/decrypted/types.go
+++ tdata/decrypted/types.go
@@ -30,173 +30,173 @@ var LSK = map[uint32]interface{}{
 }
 
 var DBI = map[uint32]string{
-	0x00: "DbiKey" ,
-	0x01: "DbiUser" ,
-	0x02: "DbiDcOptionOldOld" ,
-	0x03: "DbiChatSizeMax" ,
-	0x04: "DbiMutePeer" ,
-	0x05: "DbiSendKeyOld" ,
-	0x06: "DbiAutoStart" ,
-	0x07: "DbiStartMinimized" ,
-	0x08: "DbiSoundNotify" ,
-	0x09: "DbiWorkMode" ,
-	0x0a: "DbiSeenTrayTooltip" ,
-	0x0b: "DbiDesktopNotify" ,
-	0x0c: "DbiAutoUpdate" ,
-	0x0d: "DbiLastUpdateCheck" ,
-	0x0e: "DbiWindowPosition" ,
-	0x0f: "DbiConnectionTypeOld" ,
-	0x11: "DbiDefaultAttach" ,
-	0x12: "DbiCatsAndDogs" ,
-	0x13: "DbiReplaceEmoji" ,
-	0x14: "DbiAskDownloadPath" ,
-	0x15: "DbiDownloadPathOld" ,
-	0x16: "DbiScaleOld" ,
-	0x17: "DbiEmojiTabOld" ,
-	0x18: "DbiRecentEmojiOldOld" ,
-	0x19: "DbiLoggedPhoneNumber" ,
-	0x1a: "DbiMutedPeers" ,
-	0x1c: "DbiNotifyView" ,
-	0x1d: "DbiSendToMenu" ,
-	0x1e: "DbiCompressPastedImage" ,
-	0x1f: "DbiLangOld" ,
-	0x20: "DbiLangFileOld" ,
-	0x21: "DbiTileBackgroundOld" ,
-	0x22: "DbiAutoLock" ,
-	0x23: "DbiDialogLastPath" ,
-	0x24: "DbiRecentEmojiOld" ,
-	0x25: "DbiEmojiVariantsOld" ,
-	0x26: "DbiRecentStickers" ,
-	0x27: "DbiDcOptionOld" ,
-	0x28: "DbiTryIPv6" ,
-	0x29: "DbiSongVolume" ,
-	0x30: "DbiWindowsNotificationsOld" ,
-	0x31: "DbiIncludeMutedOld" ,
-	0x32: "DbiMegagroupSizeMax" ,
-	0x33: "DbiDownloadPath" ,
-	0x34: "DbiAutoDownload" ,
-	0x35: "DbiSavedGifsLimit" ,
-	0x36: "DbiShowingSavedGifsOld" ,
-	0x37: "DbiAutoPlay" ,
-	0x38: "DbiAdaptiveForWide" ,
-	0x39: "DbiHiddenPinnedMessages" ,
-	0x3a: "DbiRecentEmoji" ,
-	0x3b: "DbiEmojiVariants" ,
-	0x40: "DbiDialogsMode" ,
-	0x41: "DbiModerateMode" ,
-	0x42: "DbiVideoVolume" ,
-	0x43: "DbiStickersRecentLimit" ,
-	0x44: "DbiNativeNotifications" ,
-	0x45: "DbiNotificationsCount" ,
-	0x46: "DbiNotificationsCorner" ,
-	0x47: "DbiThemeKeyOld" ,
-	0x48: "DbiDialogsWidthRatioOld" ,
-	0x49: "DbiUseExternalVideoPlayer" ,
-	0x4a: "DbiDcOptions" ,
-	0x4b: "DbiMtpAuthorization" ,
-	0x4c: "DbiLastSeenWarningSeenOld" ,
-	0x4d: "DbiAuthSessionSettings" ,
-	0x4e: "DbiLangPackKey" ,
-	0x4f: "DbiConnectionType" ,
-	0x50: "DbiStickersFavedLimit" ,
-	0x51: "DbiSuggestStickersByEmoji" ,
-	0x52: "DbiSuggestEmoji" ,
-	0x53: "DbiTxtDomainString" ,
-	0x54: "DbiThemeKey" ,
-	0x55: "DbiTileBackground" ,
-	0x56: "DbiCacheSettings" ,
-	0x57: "DbiAnimationsDisabled" ,
-	0x58: "DbiScalePercent" ,
-	0x59: "DbiPlaybackSpeed" ,
-	0x5a: "DbiLanguagesKey" ,
-	333: "DbiEncryptedWithSalt" ,
-	444: "DbiEncrypted" ,
-	666: "DbiVersion" ,
+	0x00: "DbiKey",
+	0x01: "DbiUser",
+	0x02: "DbiDcOptionOldOld",
+	0x03: "DbiChatSizeMax",
+	0x04: "DbiMutePeer",
+	0x05: "DbiSendKeyOld",
+	0x06: "DbiAutoStart",
+	0x07: "DbiStartMinimized",
+	0x08: "DbiSoundNotify",
+	0x09: "DbiWorkMode",
+	0x0a: "DbiSeenTrayTooltip",
+	0x0b: "DbiDesktopNotify",
+	0x0c: "DbiAutoUpdate",
+	0x0d: "DbiLastUpdateCheck",
+	0x0e: "DbiWindowPosition",
+	0x0f: "DbiConnectionTypeOld",
+	0x11: "DbiDefaultAttach",
+	0x12: "DbiCatsAndDogs",
+	0x13: "DbiReplaceEmoji",
+	0x14: "DbiAskDownloadPath",
+	0x15: "DbiDownloadPathOld",
+	0x16: "DbiScaleOld",
+	0x17: "DbiEmojiTabOld",
+	0x18: "DbiRecentEmojiOldOld",
+	0x19: "DbiLoggedPhoneNumber",
+	0x1a: "DbiMutedPeers",
+	0x1c: "DbiNotifyView",
+	0x1d: "DbiSendToMenu",
+	0x1e: "DbiCompressPastedImage",
+	0x1f: "DbiLangOld",
+	0x20: "DbiLangFileOld",
+	0x21: "DbiTileBackgroundOld",
+	0x22: "DbiAutoLock",
+	0x23: "DbiDialogLastPath",
+	0x24: "DbiRecentEmojiOld",
+	0x25: "DbiEmojiVariantsOld",
+	0x26: "DbiRecentStickers",
+	0x27: "DbiDcOptionOld",
+	0x28: "DbiTryIPv6",
+	0x29: "DbiSongVolume",
+	0x30: "DbiWindowsNotificationsOld",
+	0x31: "DbiIncludeMutedOld",
+	0x32: "DbiMegagroupSizeMax",
+	0x33: "DbiDownloadPath",
+	0x34: "DbiAutoDownload",
+	0x35: "DbiSavedGifsLimit",
+	0x36: "DbiShowingSavedGifsOld",
+	0x37: "DbiAutoPlay",
+	0x38: "DbiAdaptiveForWide",
+	0x39: "DbiHiddenPinnedMessages",
+	0x3a: "DbiRecentEmoji",
+	0x3b: "DbiEmojiVariants",
+	0x40: "DbiDialogsMode",
+	0x41: "DbiModerateMode",
+	0x42: "DbiVideoVolume",
+	0x43: "DbiStickersRecentLimit",
+	0x44: "DbiNativeNotifications",
+	0x45: "DbiNotificationsCount",
+	0x46: "DbiNotificationsCorner",
+	0x47: "DbiThemeKeyOld",
+	0x48: "DbiDialogsWidthRatioOld",
+	0x49: "DbiUseExternalVideoPlayer",
+	0x4a: "DbiDcOptions",
+	0x4b: "DbiMtpAuthorization",
+	0x4c: "DbiLastSeenWarningSeenOld",
+	0x4d: "DbiAuthSessionSettings",
+	0x4e: "DbiLangPackKey",
+	0x4f: "DbiConnectionType",
+	0x50: "DbiStickersFavedLimit",
+	0x51: "DbiSuggestStickersByEmoji",
+	0x52: "DbiSuggestEmoji",
+	0x53: "DbiTxtDomainString",
+	0x54: "DbiThemeKey",
+	0x55: "DbiTileBackground",
+	0x56: "DbiCacheSettings",
+	0x57: "DbiAnimationsDisabled",
+	0x58: "DbiScalePercent",
+	0x59: "DbiPlaybackSpeed",
+	0x5a: "DbiLanguagesKey",
+	333:  "DbiEncryptedWithSalt",
+	444:  "DbiEncrypted",
+	666:  "DbiVersion",
 }
 
 const (
-	DbiKey = 0x00
-	DbiUser = 0x01
-	DbiDcOptionOldOld = 0x02
-	DbiChatSizeMax = 0x03
-	DbiMutePeer = 0x04
-	DbiSendKeyOld = 0x05
-	DbiAutoStart = 0x06
-	DbiStartMinimized = 0x07
-	DbiSoundNotify = 0x08
-	DbiWorkMode = 0x09
-	DbiSeenTrayTooltip = 0x0a
-	DbiDesktopNotify = 0x0b
-	DbiAutoUpdate = 0x0c
-	DbiLastUpdateCheck = 0x0d
-	DbiWindowPosition = 0x0e
-	DbiConnectionTypeOld = 0x0f
-	DbiDefaultAttach = 0x11
-	DbiCatsAndDogs = 0x12
-	DbiReplaceEmoji = 0x13
-	DbiAskDownloadPath = 0x14
-	DbiDownloadPathOld = 0x15
-	DbiScaleOld = 0x16
-	DbiEmojiTabOld = 0x17
-	DbiRecentEmojiOldOld = 0x18
-	DbiLoggedPhoneNumber = 0x19
-	DbiMutedPeers = 0x1a
-	DbiNotifyView = 0x1c
-	DbiSendToMenu = 0x1d
-	DbiCompressPastedImage = 0x1e
-	DbiLangOld = 0x1f
-	DbiLangFileOld = 0x20
-	DbiTileBackgroundOld = 0x21
-	DbiAutoLock = 0x22
-	DbiDialogLastPath = 0x23
-	DbiRecentEmojiOld = 0x24
-	DbiEmojiVariantsOld = 0x25
-	DbiRecentStickers = 0x26
-	DbiDcOptionOld = 0x27
-	DbiTryIPv6 = 0x28
-	DbiSongVolume = 0x29
+	DbiKey                     = 0x00
+	DbiUser                    = 0x01
+	DbiDcOptionOldOld          = 0x02
+	DbiChatSizeMax             = 0x03
+	DbiMutePeer                = 0x04
+	DbiSendKeyOld              = 0x05
+	DbiAutoStart               = 0x06
+	DbiStartMinimized          = 0x07
+	DbiSoundNotify             = 0x08
+	DbiWorkMode                = 0x09
+	DbiSeenTrayTooltip         = 0x0a
+	DbiDesktopNotify           = 0x0b
+	DbiAutoUpdate              = 0x0c
+	DbiLastUpdateCheck         = 0x0d
+	DbiWindowPosition          = 0x0e
+	DbiConnectionTypeOld       = 0x0f
+	DbiDefaultAttach           = 0x11
+	DbiCatsAndDogs             = 0x12
+	DbiReplaceEmoji            = 0x13
+	DbiAskDownloadPath         = 0x14
+	DbiDownloadPathOld         = 0x15
+	DbiScaleOld                = 0x16
+	DbiEmojiTabOld             = 0x17
+	DbiRecentEmojiOldOld       = 0x18
+	DbiLoggedPhoneNumber       = 0x19
+	DbiMutedPeers              = 0x1a
+	DbiNotifyView              = 0x1c
+	DbiSendToMenu              = 0x1d
+	DbiCompressPastedImage     = 0x1e
+	DbiLangOld                 = 0x1f
+	DbiLangFileOld             = 0x20
+	DbiTileBackgroundOld       = 0x21
+	DbiAutoLock                = 0x22
+	DbiDialogLastPath          = 0x23
+	DbiRecentEmojiOld          = 0x24
+	DbiEmojiVariantsOld        = 0x25
+	DbiRecentStickers          = 0x26
+	DbiDcOptionOld             = 0x27
+	DbiTryIPv6                 = 0x28
+	DbiSongVolume              = 0x29
 	DbiWindowsNotificationsOld = 0x30
-	DbiIncludeMutedOld = 0x31
-	DbiMegagroupSizeMax = 0x32
-	DbiDownloadPath = 0x33
-	DbiAutoDownload = 0x34
-	DbiSavedGifsLimit = 0x35
-	DbiShowingSavedGifsOld = 0x36
-	DbiAutoPlay = 0x37
-	DbiAdaptiveForWide = 0x38
-	DbiHiddenPinnedMessages = 0x39
-	DbiRecentEmoji = 0x3a
-	DbiEmojiVariants = 0x3b
-	DbiDialogsMode = 0x40
-	DbiModerateMode = 0x41
-	DbiVideoVolume = 0x42
-	DbiStickersRecentLimit = 0x43
-	DbiNativeNotifications = 0x44
-	DbiNotificationsCount = 0x45
-	DbiNotificationsCorner = 0x46
-	DbiThemeKeyOld = 0x47
-	DbiDialogsWidthRatioOld = 0x48
-	DbiUseExternalVideoPlayer = 0x49
-	DbiDcOptions = 0x4a
-	DbiMtpAuthorization = 0x4b
-	DbiLastSeenWarningSeenOld = 0x4c
-	DbiAuthSessionSettings = 0x4d
-	DbiLangPackKey = 0x4e
-	DbiConnectionType = 0x4f
-	DbiStickersFavedLimit = 0x50
-	DbiSuggestStickersByEmoji = 0x51
-	DbiSuggestEmoji = 0x52
-	DbiTxtDomainString = 0x53
-	DbiThemeKey = 0x54
-	DbiTileBackground = 0x55
-	DbiCacheSettings = 0x56
-	DbiAnimationsDisabled = 0x57
-	DbiScalePercent = 0x58
-	DbiPlaybackSpeed = 0x59
-	DbiLanguagesKey = 0x5a
-	DbiEncryptedWithSalt = 333
-	DbiEncrypted = 444
-	DbiVersion = 666
+	DbiIncludeMutedOld         = 0x31
+	DbiMegagroupSizeMax        = 0x32
+	DbiDownloadPath            = 0x33
+	DbiAutoDownload            = 0x34
+	DbiSavedGifsLimit          = 0x35
+	DbiShowingSavedGifsOld     = 0x36
+	DbiAutoPlay                = 0x37
+	DbiAdaptiveForWide         = 0x38
+	DbiHiddenPinnedMessages    = 0x39
+	DbiRecentEmoji             = 0x3a
+	DbiEmojiVariants           = 0x3b
+	DbiDialogsMode             = 0x40
+	DbiModerateMode            = 0x41
+	DbiVideoVolume             = 0x42
+	DbiStickersRecentLimit     = 0x43
+	DbiNativeNotifications     = 0x44
+	DbiNotificationsCount      = 0x45
+	DbiNotificationsCorner     = 0x46
+	DbiThemeKeyOld             = 0x47
+	DbiDialogsWidthRatioOld    = 0x48
+	DbiUseExternalVideoPlayer  = 0x49
+	DbiDcOptions               = 0x4a
+	DbiMtpAuthorization        = 0x4b
+	DbiLastSeenWarningSeenOld  = 0x4c
+	DbiAuthSessionSettings     = 0x4d
+	DbiLangPackKey             = 0x4e
+	DbiConnectionType          = 0x4f
+	DbiStickersFavedLimit      = 0x50
+	DbiSuggestStickersByEmoji  = 0x51
+	DbiSuggestEmoji            = 0x52
+	DbiTxtDomainString         = 0x53
+	DbiThemeKey                = 0x54
+	DbiTileBackground          = 0x55
+	DbiCacheSettings           = 0x56
+	DbiAnimationsDisabled      = 0x57
+	DbiScalePercent            = 0x58
+	DbiPlaybackSpeed           = 0x59
+	DbiLanguagesKey            = 0x5a
+	DbiEncryptedWithSalt       = 333
+	DbiEncrypted               = 444
+	DbiVersion                 = 666
 )
 
 func ReverseLSK(a interface{}) uint32 {
@@ -292,27 +292,27 @@ type Audios struct {
 }
 
 type ReportSpamStatuses struct {
-	FullLen uint32
-	Size int32 `struc:"sizeof=ReportSpamStatuses"`
+	FullLen            uint32
+	Size               int32 `struc:"sizeof=ReportSpamStatuses"`
 	ReportSpamStatuses []ReportSpamStatus
 }
-type ReportSpamStatus struct{
-	Peer uint64
+type ReportSpamStatus struct {
+	Peer   uint64
 	Status int32
 }
 
-type UserMap struct {}
+type UserMap struct{}
 
-type Draft struct {}
+type Draft struct{}
 
-type DraftPosition struct {}
+type DraftPosition struct{}
 
-type RecentStickersOld struct {}
+type RecentStickersOld struct{}
 
-type BackgroundOld struct {}
+type BackgroundOld struct{}
 
 type UserSettings struct {
-	FullLen uint32
+	FullLen           uint32
 	DbiDcOptionOldOld struct {
 		DcId uint32
 		Host string
@@ -321,176 +321,176 @@ type UserSettings struct {
 	}
 	DbiDcOptionOld struct {
 		DcIdWithShift uint32
-		Flags int32
-		IP    string
-		Port  uint32
+		Flags         int32
+		IP            string
+		Port          uint32
 	}
 	DbiDcOptions struct {
 		Serialized []byte
 	}
-	DbiChatSizeMax int32
-	DbiSavedGifsLimit int32
+	DbiChatSizeMax         int32
+	DbiSavedGifsLimit      int32
 	DbiStickersRecentLimit int32
-	DbiStickersFavedLimit int32
-	DbiMegagroupSizeMax int32
-	DbiUser struct {
+	DbiStickersFavedLimit  int32
+	DbiMegagroupSizeMax    int32
+	DbiUser                struct {
 		UserId int32
-		DcId uint32
+		DcId   uint32
 	}
 	DbiKey struct {
 		DcId int32
-		Key []byte
+		Key  []byte
 	}
 	DbiMtpAuthorization struct {
 		Serialized []byte
 	}
-	DbiAutoStart int32
-	DbiStartMinimized int32
-	DbiSendToMenu int32
+	DbiAutoStart              int32
+	DbiStartMinimized         int32
+	DbiSendToMenu             int32
 	DbiUseExternalVideoPlayer int32
-	DbiCacheSettings struct {
+	DbiCacheSettings          struct {
 		Size int64
 		Time int32
 	}
 	DbiAnimationsDisabled int32
-	DbiSoundNotify int32
-	DbiAutoDownload struct {
+	DbiSoundNotify        int32
+	DbiAutoDownload       struct {
 		Photo int32
 		Audio int32
-		Gif int32
+		Gif   int32
 	}
-	DbiAutoPlay int32
+	DbiAutoPlay    int32
 	DbiDialogsMode struct {
 		Enabled int32
 		ModeInt int32
 	}
-	DbiModerateMode int32
-	DbiIncludeMutedOld int32
-	DbiShowingSavedGifsOld int32
-	DbiDesktopNotify int32
+	DbiModerateMode            int32
+	DbiIncludeMutedOld         int32
+	DbiShowingSavedGifsOld     int32
+	DbiDesktopNotify           int32
 	DbiWindowsNotificationsOld int32
-	DbiNativeNotifications int32
-	DbiNotificationsCount int32
-	DbiNotificationsCorner int32
-	DbiDialogsWidthRatioOld int32
-	DbiLastSeenWarningSeenOld int32
-	DbiAuthSessionSettings struct {
+	DbiNativeNotifications     int32
+	DbiNotificationsCount      int32
+	DbiNotificationsCorner     int32
+	DbiDialogsWidthRatioOld    int32
+	DbiLastSeenWarningSeenOld  int32
+	DbiAuthSessionSettings     struct {
 		V []byte
 	}
-	DbiWorkMode int32
-	DbiTxtDomainString string
+	DbiWorkMode          int32
+	DbiTxtDomainString   string
 	DbiConnectionTypeOld struct {
-		V int32
-		Host string
-		Port int32
-		User string
+		V        int32
+		Host     string
+		Port     int32
+		User     string
 		Password string
 	}
 	DbiConnectionType int32 //TODO
-	DbiThemeKeyOld uint64
-	DbiThemeKey struct{
-		KeyDay uint64
-		KeyNight uint64
+	DbiThemeKeyOld    uint64
+	DbiThemeKey       struct {
+		KeyDay    uint64
+		KeyNight  uint64
 		NightMode uint32
 	}
-	DbiLangPackKey uint64
-	DbiLanguagesKey uint64
-	DbiTryIPv6 int32
+	DbiLangPackKey     uint64
+	DbiLanguagesKey    uint64
+	DbiTryIPv6         int32
 	DbiSeenTrayTooltip int32
-	DbiAutoUpdate int32
+	DbiAutoUpdate      int32
 	DbiLastUpdateCheck int32
-	DbiScaleOld int32
-	DbiScalePercent int32
-	DbiLangOld int32
-	DbiLangFileOld string
-	DbiWindowPosition struct {
-		X int
-		Y int
-		W int
-		H int
-		Moncrc int32
+	DbiScaleOld        int32
+	DbiScalePercent    int32
+	DbiLangOld         int32
+	DbiLangFileOld     string
+	DbiWindowPosition  struct {
+		X         int
+		Y         int
+		W         int
+		H         int
+		Moncrc    int32
 		Maximized int
 	}
 	DbiLoggedPhoneNumber string
-	DbiMutePeer uint64
-	DbiMutedPeers struct {
+	DbiMutePeer          uint64
+	DbiMutedPeers        struct {
 		Count uint32 `struc:"sizeof="Peers"`
 		Peers []uint64
 	}
-	DbiSendKeyOld int32
-	DbiCatsAndDogs int32
+	DbiSendKeyOld        int32
+	DbiCatsAndDogs       int32
 	DbiTileBackgroundOld int32
-	DbiTileBackground struct {
-		TileDay int32
+	DbiTileBackground    struct {
+		TileDay   int32
 		TileNight int32
 	}
-	DbiAdaptiveForWide int32
-	DbiAutoLock int32
-	DbiReplaceEmoji int32
-	DbiSuggestEmoji int32
+	DbiAdaptiveForWide        int32
+	DbiAutoLock               int32
+	DbiReplaceEmoji           int32
+	DbiSuggestEmoji           int32
 	DbiSuggestStickersByEmoji int32
-	DbiDefaultAttach int32
-	DbiNotifyView int32
-	DbiAskDownloadPath int32
-	DbiDownloadPathOld string
-	DbiDownloadPath struct {
-		V string
+	DbiDefaultAttach          int32
+	DbiNotifyView             int32
+	DbiAskDownloadPath        int32
+	DbiDownloadPathOld        string
+	DbiDownloadPath           struct {
+		V        string
 		Bookmark []byte
 	}
 	DbiCompressPastedImage int32
-	DbiEmojiTabOld int32
-	DbiRecentEmojiOldOld []struct{
-		First uint32
+	DbiEmojiTabOld         int32
+	DbiRecentEmojiOldOld   []struct {
+		First  uint32
 		Second uint16
 	}
-	DbiRecentEmojiOld []struct{
-		First uint64
+	DbiRecentEmojiOld []struct {
+		First  uint64
 		Second uint16
 	}
-	DbiRecentEmoji []struct{
-		First string
+	DbiRecentEmoji []struct {
+		First  string
 		Second uint16
 	}
-	DbiRecentStickers []struct{
-		First uint64
+	DbiRecentStickers []struct {
+		First  uint64
 		Second uint16
 	}
-	DbiEmojiVariantsOld []struct{
-		First uint32
+	DbiEmojiVariantsOld []struct {
+		First  uint32
 		Second uint64
 	}
-	DbiEmojiVariants []struct{
-		First string
+	DbiEmojiVariants []struct {
+		First  string
 		Second uint16
 	}
-	DbiHiddenPinnedMessages []struct{
+	DbiHiddenPinnedMessages []struct {
 		PeerId uint64
-		MsgId int32
+		MsgId  int32
 	}
 	DbiDialogLastPath string
-	DbiSongVolume int32
-	DbiVideoVolume int32
-	DbiPlaybackSpeed int32
+	DbiSongVolume     int32
+	DbiVideoVolume    int32
+	DbiPlaybackSpeed  int32
 }
 
-type RecentHashtagsAndBots struct {}
+type RecentHashtagsAndBots struct{}
 
-type StickersOld struct {}
+type StickersOld struct{}
 
-type SavedPeers struct {}
+type SavedPeers struct{}
 
-type SavedGifsOld struct {}
+type SavedGifsOld struct{}
 
-type SavedGifs struct {}
+type SavedGifs struct{}
 
-type StickersKeys struct {}
+type StickersKeys struct{}
 
-type TrustedBots struct {}
+type TrustedBots struct{}
 
-type FavedStickers struct {}
+type FavedStickers struct{}
 
-type ExportSettings struct {}
+type ExportSettings struct{}
 
-type Background struct {}
+type Background struct{}
 
-type SelfSerialized struct {}
+type SelfSerialized struct{}
